7bd038203359d47d627e3b5a81c043b775c8b490
olen cook <olendvcook@gmail.com>
2013-02-11 17:55:33 -0700

    Initial algorithm for determining able space units can go

--- src/units/UnitPath.java
+++ src/units/UnitPath.java
@@ -6,12 +6,13 @@ import pack.NULLwars.main.MainGamePanel;
 import map.GameBoard;
 import map.TileMap;
 import android.graphics.Canvas;
+import android.util.Log;
 
 public class UnitPath {
 	
 	private GameBoard board;
 	private  int unitType, x, y, moveRange;
-	private ArrayList[][] ablePaths;
+	private int[][] ablePaths;
 	
 	public UnitPath() {
 		unitType = 10;
@@ -20,20 +21,41 @@ public class UnitPath {
 		y = 2;
 		
 	}
-	public void draw(Canvas canvas) {
-		
+	public void draw(Canvas canvas, int x, int y, int blockSize, boolean alias) {
+
 		
 	}
 	public void update() {
 		
 		
 	}
-	public void createPath(TileMap tilemap, int x, int y, int unitType, int moveRange) {
-		ablePaths = new ArrayList[2][0]; 
+	public void createPath(int x, int y, int unitType, int moveRange) {
+		int tempRange = 0;
+		int maxBlocks = (2 * (moveRange * moveRange)) + (2 * moveRange) + 1;
+		ablePaths = new int[maxBlocks][2]; 
+		
+		//fill in a diamond formation
+		int counter = 0;
+		for (int row = -moveRange; row <= moveRange; row++){
+			for (int col = -moveRange; col <= moveRange; col++){
+				if (col == 0){
+					for (int i = -(tempRange); i <= tempRange; i++){
+						ablePaths[counter][0] = col+i;
+						ablePaths[counter][1] = row;
+						//Log.d("x: ", ablePaths[0][0]+"");
+						//Log.d("y: ", ablePaths[0][1]+"");
+						counter++;
+					}
+					if (row < 0) tempRange++;
+					else if (row >= 0) tempRange--;
+					//Log.d("counter: ", counter+"");
+				}
+			}
+		}
 		
 	}
-	public int getFucks() {
-		return -8;	
+	public int[][] getPath() {
+		return ablePaths;
 	}
 
 }
